352524441340379ddd4bc87b076b1a85b777aae7
ayush17042007 <ayushporwall17@gmail.com>
2026-06-23 23:39:40 +0530

    Backend integration with Frontend

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://codevector-product-explorer.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
